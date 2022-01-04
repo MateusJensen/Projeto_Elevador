4de86cda32a9e2ef3aeaf4fdfec204fcf33dba3b
MateusJensen <mateusjensen05@gmail.com>
2022-01-04 15:42:12 -0300

    Projeto Elevador
    Adicionando projeto elevador em C#

---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,89 @@
+﻿using ProjetoElevador.Models;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int andares = Elevador.andares();
+            int capacidade = Elevador.capacidade();
+            int pessoas = Elevador.pessoas(capacidade);
+            Elevador.inicializar(0);
+            int andar_atual = 0;
+            
+            Console.WriteLine(
+                    "\n1 - Subir um andar\n" +
+                    "2 - Descer um andar\n" +
+                    "3 - Adicionar uma pessoa ao elevador\n" +
+                    "4 - Remover uma pessoa do elevador\n" +
+                    "5 - Parar elevador e sair\n");
+            int  answer = int.Parse(Console.ReadLine());
+
+            while (answer != 5)
+            {
+                if (answer == 1)
+                {
+                    andar_atual = Elevador.subir(andar_atual, andares);
+                    Console.WriteLine($"Andar atual: {andar_atual}");
+                    Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+                    answer = int.Parse(Console.ReadLine());
+                }
+                else if (answer == 2)
+                {
+                    andar_atual = Elevador.descer(andar_atual, andares);
+                    Console.WriteLine($"Andar atual: {andar_atual}");
+                    Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+                    answer = int.Parse(Console.ReadLine());
+                }
+                else if (answer == 3)
+                {
+                    pessoas = Elevador.entrar(pessoas, capacidade);
+                    Console.WriteLine($"Pessoas no elevador: {pessoas}");
+                    Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+                    answer = int.Parse(Console.ReadLine());
+                }
+                else if (answer == 4)
+                {
+                    pessoas = Elevador.sair(pessoas);
+                    Console.WriteLine($"Pessoas no elevador: {pessoas}");
+                    Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+                    answer = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Selecione uma opção válida!");
+                    Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+                    answer = int.Parse(Console.ReadLine());
+
+                }
+            }
+        }
+    }
+}
